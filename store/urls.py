87c2bc9d66b3ee6c8023cfd559cbae67db81c6a0
arasopraza <aras.opraza@gmail.com>
2024-08-06 16:23:03 +0700

    feat: add class based view

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,7 @@
-from rest_framework_nested import routers
+from django.urls import path
 from . import views
 
-router = routers.DefaultRouter()
-router.register('products', views.ProductViewSet, basename='products')
-
-products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
-
-# URLConf
-urlpatterns = router.urls + products_router.urls
+urlpatterns = [
+  path('products/',views.ProductList.as_view()),
+  path('products/<int:pk>/', views.ProductDetail.as_view()),
+]
