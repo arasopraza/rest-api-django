5ace4c5cd3f1dc981fd34bddf5650ad57967d473
arasopraza <aras.opraza@gmail.com>
2024-08-08 10:19:41 +0700

    feat: add function based view

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('products/',views.ProductList.as_view()),
-  path('products/<int:pk>/', views.ProductDetail.as_view()),
+  path('products/',views.product_list),
 ]
